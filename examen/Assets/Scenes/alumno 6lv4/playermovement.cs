5565385b965b223fb3c4ca5998c0520c554b3429
Francesca Vera <sally.acorn16@hotmail.com>
2023-05-24 18:49:55 -0500

    francesca o.v. movimientos basicos jugador

---
+++ examen/Assets/Scenes/alumno 6lv4/playermovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector2 movedirection;
+    private Vector2 mousePosition;
+    public Camera sceneCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+    void FixedUpdate() // sirve más para calcular fisicas
+    {
+        move();
+    }
+    void ProcessInputs()//esto ba despues en void update
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        //llamo a los inputs mismos del unity para ahorrarme especificar que teclas sirven para mover
+        movedirection = new Vector2(moveX, moveY);
+        mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
+        //screen to world point es lo que nos permite identificar donde esta el mouse en la pantalla gracias a la cámara :D
+    }
+
+    //get axis nos da un valor entre 0  y 1 incluyendo decimales y al agregarle raw solo nos da 1 o 0 sin decimales
+    void move()
+
+    {
+        rb.velocity = new Vector2(movedirection.x * moveSpeed, movedirection.y * moveSpeed);
+        //esto nos dejara que el player rote con el movimiento del mouse
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        // Mathf.Rad2Deg es simplemente una constante para realizar conversiones de radianes a grados en Unity.
+        //mathf.atan2 calcula el ángulo en radianes entre el eje x
+        rb.rotation = aimAngle;
+        //aim angle y aim direction, como su nombre lo indica nos ayudan a calcular el angulo y dirección, unity mismo las tiene :D
+    }
+
+}
+
+
+
