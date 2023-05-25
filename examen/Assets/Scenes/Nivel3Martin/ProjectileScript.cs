fdfe1b48fac1dbad61edd3f440b1694095c66c93
AiLaicNothing <ailaicnothing@gmail.com>
2023-05-24 19:32:25 -0500

    asd

--- examen/Assets/Scenes/Nivel3Martin/ProjectileScript.cs
+++ examen/Assets/Scenes/Nivel3Martin/ProjectileScript.cs
@@ -16,7 +16,7 @@ public class ProjectileScript : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("Player");
         Vector2 Dir = (target.transform.position - transform.position).normalized * speed;
         Rb2D.velocity = new Vector2(Dir.x,Dir.y);
-        Counter.
+        
         Destroy(this.gameObject, 2);
 
     }
